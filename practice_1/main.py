c20c824575734959dad9c52bd1a943828819b3be
Dastan <ddbolatkhan@gmail.com>
2025-09-01 23:52:49 +0500

    Try to do mini crud

--- practice_1/main.py
+++ practice_1/main.py
@@ -1,4 +1,7 @@
+from typing import Union
 from fastapi import FastAPI
+
+from pydantic import BaseModel
 
 from enum import Enum
 
@@ -7,10 +10,6 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello Dastan"}
-
-@app.get("/users/{user_id}")
-async def get_user(user_id: int, is_active: bool = True):
-    return {"user_id": user_id, "active_status": is_active}
 
 # Try to write Enum
 class ModelName(str, Enum):
@@ -22,6 +21,73 @@ class ModelName(str, Enum):
 async def get_model(model_name: ModelName):
     if model_name is ModelName.alexnet:
         return {"model_name": model_name, "message": "Deep lerning FTW!"}
-    elif model_name is ModelName.lenet:
+    elif model_name.value == "lenet":
         return {"model_name": model_name, "message": "LeCNN all the images"}
     return {"model_name": model_name, "message": "Have some residuals"}
+
+# Write query params
+
+@app.get("/items/{item_id}")
+async def get_items(item_id: int, needed: str, not_needed: str | None = None, is_enable: bool = True, stery: str | None = None):
+    if not_needed:
+        return {"item_id": item_id, "needed": needed, "not_needed": not_needed, "enable_status": is_enable, "just": stery}
+    return {"item_id": item_id, "needed": needed, "enable_status": is_enable, "just": stery}
+
+
+class Item(BaseModel):
+    name: str
+    quantity: int
+    price: float # bigger than 0 at least
+    description: Union[str, None] = None
+    is_active: bool = False
+
+@app.post("/items/{user_id}")
+async def create_items(user_id: int, item: Item):
+    item_dict = item.model_dump()
+    if item.quantity > 0:
+        all_price = item.quantity * item.price
+        item_dict.update({"all_items_price": all_price})
+    return item_dict
+
+# Mini crud
+data = {
+    1: {"name": "Alice", "age": 25},
+    2: {"name": "Bob", "age": 30},
+    3: {"name": "Charlie", "age": 22},
+}
+
+class User(BaseModel):
+    id: int
+    name: str
+    age: int
+
+@app.get("/users/")
+async def get_all_users():
+    return data
+
+@app.get("/users/{user_id}")
+async def get_user(user_id: int):
+    # if user_id in data:
+    #     return data[user_id]
+    return data.get(user_id)
+    
+@app.post("/users/")
+async def create_user(user: User):
+    # user_dict = user_data.model_dump()
+    # data.update(user_dict)
+    # return data
+    data[user.id] = {"name": user.name, "age": user.age}
+    return data[user.id]
+
+@app.put("/users/{user_id}")
+async def update_user(user_id: int, user: User):
+    data[user_id] = {"name": user.name, "age": user.age}
+    return data[user_id]
+
+@app.delete("/users/{user_id}")
+async def delete_user(user_id: int):
+    if user_id in data:
+        deleted = data.pop(user_id)
+        return deleted
+
+    
